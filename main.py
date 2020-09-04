91c5698144ccf9b9ab9391443111a12e3f1379af
miktuy <miktuy@mail.ru>
2020-09-04 12:23:36 +0300

    added code from second lesson

--- main.py
+++ main.py
@@ -1,20 +1,53 @@
 import requests
+import csv
 from bs4 import BeautifulSoup
+from dataclasses import dataclass
 
 
-def get_html(url: str):
+@dataclass
+class Plugin:
+    name: str
+    url: str
+    rating: int
+
+    def __iter__(self):
+        return iter((self.name, self.url, self.rating))
+
+
+def get_html(url: str) -> str:
     r = requests.get(url)
     return r.text
 
 
+def refined(row_string: str) -> int:
+    r = row_string.split()[0].replace(",", "")
+    return int(r)
+
+
+def write_csv(data: Plugin):
+    with open("plugins.csv", "a") as f:
+        writer = csv.writer(f)
+        writer.writerow(data)
+
+
 def get_data(html: str):
     soup = BeautifulSoup(html, "lxml")
-    h1 = soup.find("div", id="home-welcome").find("header").find("h1").text
-    return h1
+    popular = soup.find_all("section")[1]
+    plugins = popular.find_all("article")
+
+    for plugin in plugins:
+        name = plugin.find("h3").text
+        url = plugin.find("h3").find("a").get("href")
+        rating_string = plugin.find("span", class_="rating-count").find("a").text
+        rating = refined(rating_string)
+        data = Plugin(name, url, rating)
+        write_csv(data)
+
+    return None
 
 
 def main():
-    url = "https://wordpress.org/"
+    url = "https://wordpress.org/plugins/"
     print(get_data(get_html(url)))
 
 
